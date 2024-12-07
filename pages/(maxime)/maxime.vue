bf40a05cb2478a49579e5948937da0173c578cf8
Maxime MARIN <meltoz@outlook.fr>
2024-12-07 19:43:31 +0100

    Initial commit

---
+++ pages/(maxime)/maxime.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="text-3xl">Bouh</h1>
+  <NuxtLink to="/">Home</NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Je suis sur la page</h1>
+  <NuxtLink to="/maxime" >Maxime</NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
