6421cc53bbaa7cd1d0eb114f662b266aac6bc595
Meltoz <meltoz@outlook.fr>
2024-12-08 11:35:54 +0100

    relation avec l'API ok

--- pages/(maxime)/maxime.vue
+++ pages/(maxime)/maxime.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+  import type {Car} from "~/models/car";
+  const config = useRuntimeConfig();
+
+  const cars = ref<Car[]>([])
+
+  async function getAllUsers(): Promise<Car[]> {
+    return await $fetch('/users', {
+      baseURL: config.public.apiBaseUrl,
+    });
+  }
+
+  onMounted(async () => {
+    cars.value = await getAllUsers();
+  })
 
 </script>
 
 <template>
   <h1 class="text-3xl">Bouh</h1>
+
+  <div v-for="car in cars" :key="car.id">
+    {{ car.id }} - {{car.createdAt}}
+  </div>
   <NuxtLink to="/">Home</NuxtLink>
 </template>
-
-<style scoped>
-
-</style>
